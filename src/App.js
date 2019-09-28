85f1b2ba36c10ab602e55caa784e66bac732b699
Oleg <mfzpro@gmail.com>
2019-09-29 01:27:07 +0300

    navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 import AsyncRoute from 'preact-async-route';
 import Header from 'components/Header/Header';
 import Navbar from 'components/Navbar/Navbar';
+import s from './styles/App.scss';
 
-const getHomeLayout = () => {
-  return import(/* webpackChunkName: "HomeLayout" */ 'layouts/HomeLayout').then(m => m.default)
-};
+class App extends Component {
 
-const getAccountLayout = () => {
-  return import(/* webpackChunkName: "AccountLayout" */ 'layouts/AccountLayout').then(m => m.default)
-};
+  componentWillMount() {
+    this.removeCss = s._insertCss();
+  }
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Router>
-        <AsyncRoute path='/' getComponent={getHomeLayout}/>
-        <AsyncRoute path='/account' getComponent={getAccountLayout}/>
-      </Router>
-      <Navbar />
-    </div>
-  );
+  componentWillUnmount() {
+    this.removeCss();
+  }
+
+  render() {
+    const getHomeLayout = () => {
+      return import(/* webpackChunkName: "HomeLayout" */ 'layouts/HomeLayout').then(m => m.default)
+    };
+    
+    const getAccountLayout = () => {
+      return import(/* webpackChunkName: "AccountLayout" */ 'layouts/AccountLayout').then(m => m.default)
+    };
+    
+    return (
+      <div class={s.root}>
+        <Header />
+        <Router>
+          <AsyncRoute path='/' getComponent={getHomeLayout}/>
+          <AsyncRoute path='/account' getComponent={getAccountLayout}/>
+        </Router>
+        <Navbar />
+      </div>
+    );
+  }
 }
 
 export default App;
