ff7a6e132fb5b0216c8a77c62bb11fb2f8573cd0
d.s.pustovit <ditrix1993@gmail.com>
2019-09-28 12:07:48 +0500

    path

--- src/store/entity.js
+++ src/store/entity.js
@@ -2,6 +2,7 @@ import bind from 'autobind-decorator';
 import { observable, action } from 'mobx';
 import axios from 'axios';
 const path = process.env.path;
+console.log(path)
 
 @bind
 class EntityStore {
@@ -9,7 +10,7 @@ class EntityStore {
 
   @action
   load() {
-    axios.get(path || 'http://localhost:3000')
+    axios.get(path || '194.58.104.250:3000')
     .then(({ data }) => {
       this.entity = data.entity;
     })
