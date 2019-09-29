ea6583af2edf5fa8639a5f9ea574a110beffcd79
d.s.pustovit <ditrix1993@gmail.com>
2019-09-29 12:34:41 +0500

    photos

--- src/components/Participants/Participants.js
+++ src/components/Participants/Participants.js
@@ -1,11 +1,12 @@
 import { h, Component } from 'preact';
 import { observer, inject } from 'mobx-preact';
+import s from './Participants'
 
 
 @inject('participantsStore')
 @observer
 class Participants extends Component {
-/*
+
   componentWillMount() {
     this.removeCss = s._insertCss();
   }
@@ -13,15 +14,16 @@ class Participants extends Component {
   componentWillUnmount() {
     this.removeCss();
   }
-*/
+
   componentDidMount() {
     this.props.participantsStore.load();
   }
 
   render({ participantsStore }) {
-    const { entity: ee } = participantsStore;
+    const { participants } = participantsStore;
     return (
-      <div >
+      <div class={s.wrap}>
+        {participants.map(({photo_100}) => <img class={s.img} src={photo_200} />)}
       </div>
     );
   }
